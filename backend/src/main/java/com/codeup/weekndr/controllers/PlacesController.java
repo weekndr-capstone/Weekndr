f10b2774e78d9f4c5da43bce45b922bbf2446ad0
Lexi Morgan <leximorgan816@gmail.com>
2019-03-11 15:49:07 -0500

    created places controller

---
+++ backend/src/main/java/com/codeup/weekndr/controllers/PlacesController.java
@@ -0,0 +1,37 @@
+package com.codeup.weekndr.controllers;
+import com.codeup.weekndr.models.Place;
+import com.codeup.weekndr.repositories.PlaceRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+
+@RestController
+public class PlacesController {
+
+    @Autowired
+    private PlaceRepository placeDao;
+
+
+    @GetMapping("/places")
+    public Iterable<Place> placeList(){
+        return placeDao.findAll();
+    }
+
+   @GetMapping("/place")
+   public Place onePlace(@RequestParam(name="id") long id){
+        return placeDao.findById(id);
+   }
+
+   @PostMapping("/place")
+    public void savePlace(Place place){
+        placeDao.save(place);
+   }
+
+
+
+
+// closes the class:
+}
