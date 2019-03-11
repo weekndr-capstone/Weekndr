14e3c1da1da52a6981c6c3404bb1393cc45320c0
allenjustinblanco <blanco.allenjustin@gmail.com>
2019-03-11 15:51:18 -0500

    Rework without Optional

--- backend/src/main/java/com/codeup/weekndr/controllers/UsersController.java
+++ backend/src/main/java/com/codeup/weekndr/controllers/UsersController.java
@@ -2,9 +2,10 @@ package com.codeup.weekndr.controllers;
 
 import com.codeup.weekndr.models.User;
 import com.codeup.weekndr.repositories.UserRepository;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.Optional;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UsersController {
@@ -21,7 +22,7 @@ public class UsersController {
     }
 
     @GetMapping("/userlist")
-    public Optional<User> oneUser(@RequestParam long id){
+    public User oneUser(@RequestParam long id){
         return userDao.findById(id);
     }
 
