49e4763e9ff0d95217c2aff946a9d9b8d905a0f5
Landon Harvey <landonharvey5@gmail.com>
2019-03-11 15:57:25 -0500

    created VoteController

---
+++ backend/src/main/java/com/codeup/weekndr/controllers/VotesController.java
@@ -0,0 +1,35 @@
+package com.codeup.weekndr.controllers;
+
+
+import com.codeup.weekndr.models.Trip;
+import com.codeup.weekndr.models.Vote;
+import com.codeup.weekndr.repositories.TripRepository;
+import com.codeup.weekndr.repositories.VoteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class VotesController {
+
+    @Autowired
+    private VoteRepository voteDao;
+
+    @GetMapping("/voteslist")
+    public
+    Iterable<Vote> tripList(){
+        return voteDao.findAll();
+    }
+
+    @GetMapping("/trip/{id}")
+    public Vote oneTrip(@PathVariable long id){
+        return voteDao.findById(id);
+    }
+
+    @PostMapping("/trip")
+    public void userInputed(Vote vote) {
+        voteDao.save(vote);
+    }
+}
