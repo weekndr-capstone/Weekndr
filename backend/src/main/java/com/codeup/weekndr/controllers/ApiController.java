2b2ac620a1900e6c0198a0b927a0bc63bbe5a46d
Landon Harvey <landonharvey5@gmail.com>
2019-03-12 11:17:35 -0500

    uber set up, twillio set up

--- backend/src/main/java/com/codeup/weekndr/controllers/ApiController.java
+++ backend/src/main/java/com/codeup/weekndr/controllers/ApiController.java
@@ -1,10 +1,15 @@
 package com.codeup.weekndr.controllers;
 
+import com.codeup.weekndr.models.ApiKeys;
+import com.twilio.twiml.voice.Sms;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
 
 import java.util.Arrays;
 
@@ -14,8 +19,15 @@ public class ApiController {
 
     @GetMapping("/yelpList")
     public ResponseEntity<String> yelpList(){
-        ApiKeys apiKeys = new ApiKeys("", "");
+        ApiKeys apiKeys = new ApiKeys("");
         return getyelpList(apiKeys.getYelp());
+    }
+
+    @GetMapping("/twillio")
+    public String twillio(){
+        ApiKeys apiKeys = new ApiKeys("");
+        twillioTest(apiKeys.getAuthtwillio(), apiKeys.getTwillioSID());
+        return "hi";
     }
 
     private static ResponseEntity<String> getyelpList(String bearer)
@@ -35,4 +47,16 @@ public class ApiController {
         System.out.println(result);
         return result;
     }
+
+        public void twillioTest(String Auth, String SID ) {
+            Twilio.init(Auth, SID);
+
+            Message message = Message
+                    .creator(new PhoneNumber("+14159352345"), // to
+                            new PhoneNumber("+14158141829"), // from
+                            "Where's Wallace?")
+                    .create();
+
+            System.out.println(message.getSid());
+        }
 }
