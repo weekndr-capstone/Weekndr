a34473e6ec838d34f03011faf1f543d0ee8d6745
Landon Harvey <landonharvey5@gmail.com>
2019-03-13 09:33:37 -0500

    removed redundencies

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -8,15 +8,12 @@
 <script>
 import HelloWorld from './components/HelloWorld'
 import Toolbar from './components/Toolbar'
-import SearchCard from "./components/SearchCard";
 
 export default {
   name: 'App',
   components: {
-    SearchCard,
     HelloWorld,
     Toolbar,
-      CardItem
   },
   data () {
     return {
