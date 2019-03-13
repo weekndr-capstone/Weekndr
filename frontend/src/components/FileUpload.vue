0adbc82b746c070a0575a430d8c0eafbe18f3f07
allenjustinblanco <blanco.allenjustin@gmail.com>
2019-03-13 17:48:17 -0500

    Seting up File Upload

---
+++ frontend/src/components/FileUpload.vue
@@ -0,0 +1,33 @@
+<template>
+    <v-container>
+        <form action="">
+            <input type="file" />
+            <br>
+            <v-btn id="upload">Upload</v-btn>
+        </form>
+    </v-container>
+</template>
+
+<script>
+    export default {
+        name: "FileUpload",
+        data(){
+            return{
+                file: {
+                    name: ''
+                }
+            }
+        },
+        methods: {
+            fileUpload(){
+
+                console.log()
+            }
+        }
+    }
+</script>
+
+
+<style scoped>
+
+</style>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -2,6 +2,7 @@
     <div>
         <HelloWorld/>
         <CardCarousel />
+        <FileUpload/>
         <Map/>
     </div>
 </template>
@@ -10,9 +11,11 @@
     import HelloWorld from '../components/HelloWorld'
     import CardCarousel from '../components/CardCarousel'
     import Map from "../components/Map";
+    import FileUpload from "../components/FileUpload";
 
   export default {
     components: {
+        FileUpload,
         Map,
       HelloWorld,
       CardCarousel
