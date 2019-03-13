fd2f77b1a83acf0882d601133a6dd575c28a049d
Landon Harvey <landonharvey5@gmail.com>
2019-03-13 12:12:02 -0500

    added store functionality and location services for yelp

--- frontend/src/components/SearchCard.vue
+++ frontend/src/components/SearchCard.vue
@@ -1,6 +1,5 @@
 <template xmlns:v-slot="http://www.w3.org/1999/XSL/Transform">
 <div>
-
     <v-container fill-height>
         <v-layout justify-center>
             <v-card height="350px" width="450px">
@@ -16,13 +15,13 @@
                                 ></v-text-field>
                             </v-flex>
                             <v-flex xs5 offset-xs1 class="d-inline-block">
-                                <v-menu v-model="menu2" :close-on-content-click="false" :nudge-right="40"
+                                <v-menu v-model="menu1" :close-on-content-click="false" :nudge-right="40"
                                         lazy transition="scale-transition" offset-y full-width min-width="290px">
                                     <template v-slot:activator="{ on }">
                                         <p>Start Date</p>
-                                        <v-text-field v-model="Start" label="mm/dd/yyyy" readonly v-on="on" solo></v-text-field>
+                                        <v-text-field v-model="Dates.Start" label="mm/dd/yyyy" readonly v-on="on" solo></v-text-field>
                                     </template>
-                                    <v-date-picker v-model="Start" @input="menu2 = false"></v-date-picker>
+                                    <v-date-picker v-model="Dates.Start" @input="menu1 = false"></v-date-picker>
                                 </v-menu>
                             </v-flex>
                             <v-flex xs5 class="d-inline-block">
@@ -31,18 +30,18 @@
                                     <template v-slot:activator="{ on }">
                                         <p>End Date</p>
                                         <v-text-field
-                                                v-model="End"
+                                                v-model="Dates.End"
                                                 label="mm/dd/yyyy"
                                                 readonly
                                                 v-on="on"
                                                 solo
                                         ></v-text-field>
                                     </template>
-                                    <v-date-picker v-model="End" @input="menu2 = false"></v-date-picker>
+                                    <v-date-picker v-model="Dates.End" @input="menu2 = false"></v-date-picker>
                                 </v-menu>
                             </v-flex>
                             <v-flex offset-xs8>
-                            <v-btn color="info">Search</v-btn>
+                            <v-btn @click="searchLocation()" color="info">Search</v-btn>
                             </v-flex>
                         </v-card-text>
                     </v-layout>
@@ -54,13 +53,29 @@
 </template>
 
 <script>
+    import store from '../store'
+    import router from '../router'
     export default {
         name: "SearchCard",
         data(){
             return{
                 Where: '',
-                Start: '',
-                End: ''
+                Dates:{
+                    Start: '',
+                    End: ''
+                },
+                menu1: false,
+                menu2: false
+            }
+        },
+        methods: {
+            searchLocation(){
+                store.commit('changeLocation', this.Where);
+                store.commit('changeStartDate', this.Dates.Start);
+                store.commit('changeEndDate', this.Dates.End);
+                router.push('/search');
+                console.log(store.state.location);
+                console.log(store.state.dates);
             }
         }
     }
--- frontend/src/views/Search.vue
+++ frontend/src/views/Search.vue
@@ -5,10 +5,20 @@
 </template>
 
 <script>
-
+    import store from '../store'
+    import axios from 'axios'
     export default {
         components: {
 
+        },
+        mounted() {
+                axios
+                    .get('/yelpList/'+ store.state.location)
+                    .then(res => {
+                        console.log(res)
+                    }).catch(err => {
+                        console.log(err)
+                })
         }
     }
 </script>
