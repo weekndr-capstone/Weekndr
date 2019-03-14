fd26a2b590c502883712f745909bcfb7ab030e3b
allenjustinblanco <blanco.allenjustin@gmail.com>
2019-03-13 19:47:00 -0500

    Rework styles for Sign Up and Login modals

--- frontend/src/components/Toolbar.vue
+++ frontend/src/components/Toolbar.vue
@@ -1,15 +1,17 @@
-<template>
-    <v-toolbar flat class="Navbar" clipped-left>
+<template xmlns:v-slot="http://www.w3.org/1999/XSL/Transform">
+    <v-toolbar flat class="Navbar">
         <v-toolbar-title>Weekndr</v-toolbar-title>
-        <v-layout justify-end>
+        <v-layout justify-end fill-height>
             <v-toolbar-items>
-                <v-dialog v-model="SignUp" max-width="600px">
+                <v-dialog v-model="SignUp" max-width="550px">
                     <template v-slot:activator="{ on }">
                         <v-btn flat v-on="on">Sign Up</v-btn>
                     </template>
                     <v-card>
                         <v-card-text>
                             <v-container grid-list-md>
+                                <v-toolbar-title class="display-1">Sign Up</v-toolbar-title>
+                                <br>
                                 <v-layout wrap>
                                     <v-flex xs12 sm6>
                                         <v-text-field label="First name*" required solo></v-text-field>
@@ -26,24 +28,29 @@
                                     <v-flex xs12>
                                         <v-text-field label="Password*" type="password" required solo></v-text-field>
                                     </v-flex>
+                                    <v-layout justify-start>
                                     <small>*indicates required field</small>
-                                </v-layout>
+                                    </v-layout>
+                                    <v-layout justify-end>
+                                        <v-card-actions>
+                                            <v-btn flat @click="SignUp = false">Close</v-btn>
+                                            <v-btn flat @click="SignUp = false">Sign Up</v-btn>
+                                        </v-card-actions>
+                                    </v-layout>
+                                    </v-layout>
                             </v-container>
                         </v-card-text>
-                        <v-card-actions>
-                            <v-spacer></v-spacer>
-                            <v-btn flat @click="SignUp = false">Close</v-btn>
-                            <v-btn flat @click="SignUp = false">Login</v-btn>
-                        </v-card-actions>
                     </v-card>
                 </v-dialog>
-                <v-dialog v-model="Login" max-width="600px">
+                <v-dialog v-model="Login" max-width="550px">
                     <template v-slot:activator="{ on }">
                         <v-btn flat v-on="on">Login</v-btn>
                     </template>
                     <v-card>
                         <v-card-text>
                             <v-container grid-list-md>
+                                <v-toolbar-title class="display-1">Login</v-toolbar-title>
+                                <br>
                                 <v-layout wrap>
                                     <v-flex xs12>
                                         <v-text-field label="Email Address" required solo></v-text-field>
@@ -52,14 +59,16 @@
                                         <v-text-field label="Password" type="password" required solo></v-text-field>
                                     </v-flex>
                                 </v-layout>
+                                <v-layout justify-end>
+                                    <v-card-actions>
+                                        <v-spacer></v-spacer>
+                                        <v-btn flat @click="Login = false">Close</v-btn>
+                                        <v-btn flat @click="Login = false">Login</v-btn>
+                                    </v-card-actions>
+                                </v-layout>
                             </v-container>
                         </v-card-text>
-                        <v-card-actions>
-                            <v-spacer></v-spacer>
-                            <v-btn flat @click="Login = false">Close</v-btn>
-                            <v-btn flat @click="Login = false">Login</v-btn>
-                        </v-card-actions>
-                    </v-card>
+                        </v-card>
                 </v-dialog>
             </v-toolbar-items>
         </v-layout>
