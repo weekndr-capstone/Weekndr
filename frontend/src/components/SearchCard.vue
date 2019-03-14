c4e415272cb7415a509a4e9662fe0e9affb6bbc2
Landon Harvey <landonharvey5@gmail.com>
2019-03-14 14:00:44 -0500

    fixed

--- frontend/src/components/SearchCard.vue
+++ frontend/src/components/SearchCard.vue
@@ -55,7 +55,7 @@
     import router from '../router'
     import axios from 'axios'
     
-    let today='';
+    let today= new Date();
     let dd = today.getDate();
     let mm = today.getMonth() + 1; //January is 0!
     let yyyy = today.getFullYear();
