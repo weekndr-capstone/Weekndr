6adda99b25503b65e3c9bc88670e8a87e3f1fc28
Lexi Morgan <leximorgan816@gmail.com>
2019-03-14 14:27:32 -0500

    past trip component finished

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -3,6 +3,7 @@
         <HelloWorld/>
         <FileUpload/>
         <ItineraryTabs />
+        <PastTrip/>
 
         <h1>The map should be here:</h1>
         <TripHistory/>
@@ -17,13 +18,15 @@
     import ItineraryTabs from '../components/ItineraryTabs'
     import FileUpload from "../components/FileUpload";
     import TripHistory from "../components/TripHistory";
+    import PastTrip from "../components/PastTrip";
 
   export default {
     components: {
         FileUpload,
         HelloWorld,
         ItineraryTabs,
-        TripHistory
+        TripHistory,
+        PastTrip
     }
   }
 </script>
