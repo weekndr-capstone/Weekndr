bc01b1a9b4dbb02800976c55c1a7f279298ac12e
Lexi Morgan <leximorgan816@gmail.com>
2019-03-15 10:51:10 -0500

    placeholder photo changed

--- frontend/src/components/RecommendedDestinations.vue
+++ frontend/src/components/RecommendedDestinations.vue
@@ -2,7 +2,7 @@
     <v-container>
         <v-layout row class="white-background">
             <v-flex xs3>
-                <v-img src="https://picsum.photos/305"></v-img>
+                <v-img src="https://picsum.photos/311"></v-img>
             </v-flex>
             <v-flex xs9 mx-2>
                 <v-card-title><h1>Destination City Name</h1></v-card-title>
