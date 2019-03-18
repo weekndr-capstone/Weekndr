4446b1e40c7fcd5ea265750701f83d7d0cdb7e59
Landon Harvey <landonharvey5@gmail.com>
2019-03-18 14:21:15 -0500

    fixed places data movement

--- frontend/src/components/CardCarousel.vue
+++ frontend/src/components/CardCarousel.vue
@@ -1,11 +1,11 @@
 <template>
     <v-carousel light hide-delimiters :cycle="false" height="270px" class="carousel-transparent">
         <v-carousel-item :key="i" v-for="i in 7">
-            <v-layout row>
-                <v-flex xs15 sm6 md4 :key="j" v-for="j in Results.slice((3 * (i-1)),(-26 + (3*(i))))">
-                    <CardItem :card="j" class="margin">
-                    </CardItem>
-                </v-flex>
+            <v-layout row>'
+                    <v-flex xs15 sm6 md4 :key="j" v-for="j in Results.slice((3 * (i-1)),(-Results.length + (3*(i))))">
+                        <CardItem :card="j" class="margin">
+                        </CardItem>
+                    </v-flex>
             </v-layout>
         </v-carousel-item>
     </v-carousel>
