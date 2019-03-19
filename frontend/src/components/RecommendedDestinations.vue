aed45415da3d6619cead4bebc90b2b9a370fb03a
Landon Harvey <landonharvey5@gmail.com>
2019-03-19 11:16:03 -0500

    3 buttons

--- frontend/src/components/RecommendedDestinations.vue
+++ frontend/src/components/RecommendedDestinations.vue
@@ -1,7 +1,7 @@
 <template>
         <v-card color="white">
-            <v-layout>
-                 <v-flex xs4><v-img src="https://picsum.photos/311" height="100%" contain /></v-flex>
+            <v-layout wrap>
+                 <v-flex xs4><v-img :src="getImgUrl(place.url)" height="80px" width="80px" contain /></v-flex>
                      <v-flex xs8 align-self-center class="text-xs-center">
                              <h2>{{place.name}}</h2>
                  </v-flex>
@@ -9,12 +9,19 @@
          </v-card>
 </template>
 
-
 <script>
     export default {
         name: "RecommendedDestinations",
         props:{
             place: Object,
+        },
+        methods:{
+            getImgUrl(pic) {
+                if (pic == null){
+                    return require('../assets/weekdnr_logo.svg')
+                }
+                return require('../assets/'+ pic)
+            }
         }
     }
 </script>
--- frontend/src/views/Itenerary.vue
+++ frontend/src/views/Itenerary.vue
@@ -1,6 +1,17 @@
 <template>
     <div>
         <Toolbar/>
+        <v-item-group>
+            <v-container grid-list-md>
+                <v-layout wrap>
+                    <v-flex v-for="t in tabs" :key="t.id" xs4 md4>
+                        <v-item>
+                            <RecommendedDestinations :place="t"/>
+                        </v-item>
+                    </v-flex>
+                </v-layout>
+            </v-container>
+        </v-item-group>
         <Timeline/>
     </div>
 </template>
@@ -8,12 +19,35 @@
 <script>
     import Timeline from "../components/Timeline";
     import Toolbar from '../components/Toolbar';
+    import RecommendedDestinations from '../components/RecommendedDestinations'
 
     export default {
         name: "Itenerary",
         components: {
             Timeline,
-            Toolbar
+            Toolbar,
+            RecommendedDestinations
+        },
+        data(){
+            return {
+                tabs: [
+                    {
+                        id: 1,
+                        name: 'Food',
+                        url: 'food.jpg'
+                    },
+                    {
+                        id:2,
+                        name: 'Hotel',
+                        url: 'hotel.jpg'
+                    },
+                    {
+                        id:3,
+                        name: 'Experience',
+                        url: 'experience.jpg'
+                    }
+                ]
+            }
         }
     }
 </script>
